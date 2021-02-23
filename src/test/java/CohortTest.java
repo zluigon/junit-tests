804173559d04a34e439601bf6999f89f7c6d00a9
Fernando Mendoza <fernandoza@gmail.com>
2021-02-23 10:29:57 -0600

    refactor: cleaned up the code and improved the init method

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,6 +1,5 @@
 import org.junit.Before;
 import org.junit.Test;
-import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class CohortTest {
@@ -16,43 +15,36 @@ public class CohortTest {
         cohortWOne = new Cohort();
         cohortWMany = new Cohort();
 
+        Student fer = new Student(10L, "Fer");
+        Student david = new Student(20L, "David");
 
+        fer.addGrade(100);
+        fer.addGrade(40);
+
+        david.addGrade(80);
+        david.addGrade(90);
+        cohortWOne.addStudent(fer);
+        cohortWMany.addStudent(fer);
+        cohortWMany.addStudent(david);
 
     }
 
     @Test
     public void testIfAddStudentWorks(){
-        assertEquals(0, cohortWOne.getStudents().size());
-        Student fer = new Student(1L, "Fer");
-        cohortWOne.addStudent(fer);
         assertEquals(1, cohortWOne.getStudents().size());
+        assertEquals(2, cohortWMany.getStudents().size());
     }
 
     @Test
     public void testIfGetStudentsWork(){
-        Student fer = new Student(10L, "Fer");
-        Student david = new Student(20L, "David");
-        cohortWMany.addStudent(fer);
-        cohortWMany.addStudent(david);
         assertEquals(10, cohortWMany.getStudents().get(0).getId());
         assertEquals(20, cohortWMany.getStudents().get(1).getId());
     }
 
     @Test
     public void testIfCohortAvgWorks(){
-        Student fer = new Student(10L, "Fer");
-        Student david = new Student(20L, "David");
-        fer.addGrade(100);
-        fer.addGrade(40);
-        //70
-        david.addGrade(80);
-        david.addGrade(90);
-        //85
-        cohortWMany.addStudent(fer);
-        cohortWMany.addStudent(david);
-
+        assertEquals(70, cohortWOne.getCohortAverage(), 0.5);
         assertEquals(77.5, cohortWMany.getCohortAverage(), 0.5);
-
     }
 
 
